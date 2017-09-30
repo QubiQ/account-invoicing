09ddccacfe225795d697311875bb6abc5daa7489
Marçal Isern <marsal.isern@qubiq.es>
2017-09-30 19:13:58 +0200

    [FIX] Flake 8

--- account_invoice_customer_ref_unique/tests/test_account_invoice.py
+++ account_invoice_customer_ref_unique/tests/test_account_invoice.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-from odoo.exceptions import ValidationError
 from psycopg2 import IntegrityError
 import odoo.tools
 
